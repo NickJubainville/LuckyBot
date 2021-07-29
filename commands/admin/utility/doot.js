5c7f477a45728eb0140780b409ca979d1ca313d8
NJubainville1 <njubainville@gmail.com>
2021-07-28 18:12:05 -0600

    Moved alerts to ready file
    No longer need to type out commands to trigger alerts — timers will start as soon as bot goes online

--- commands/admin/utility/doot.js
+++ commands/admin/utility/doot.js
@@ -1,25 +1,27 @@
-module.exports = {
-	name: 'doot',
-	description: 'Enables alerts for Doot raids',
-    guildOnly: true,
-	execute(message) {
-        if (message.author.id !== '89918398351290368') return;
-        setInterval(channelMessage, 60000);
-        console.log('doot timer started');
-        function channelMessage() {
-            const currentDate = new Date();
-            const dayOfWeek = currentDate.getUTCDay();
-            if (dayOfWeek == 0 || dayOfWeek == 6) {
-                const utcHour = currentDate.getUTCHours();
-                if (utcHour == 17) {
-                    const time = currentDate.getMinutes();
-                    if (time == 50) {
-                        console.log('doot doot');
-                        const guildRole = message.guild.roles.cache.find(r => r.name === 'Doot');
-                        message.channel.send(`${guildRole} runs beginning in 10 minutes, grab your keys and head to Limeedle 1! Don't forget to equip drop rate cards & gear if you can <:doot:825421374205722665>`);
-                    }
-                }
-            }
-        }
-	},
-};
+// Deprecated, new version lives in ready event file
+
+// module.exports = {
+// 	name: 'doot',
+// 	description: 'Enables alerts for Doot raids',
+//     guildOnly: true,
+// 	execute(message) {
+//         if (message.author.id !== '89918398351290368') return;
+//         setInterval(channelMessage, 60000);
+//         console.log('doot timer started');
+//         function channelMessage() {
+//             const currentDate = new Date();
+//             const dayOfWeek = currentDate.getUTCDay();
+//             if (dayOfWeek == 0 || dayOfWeek == 6) {
+//                 const utcHour = currentDate.getUTCHours();
+//                 if (utcHour == 17) {
+//                     const time = currentDate.getMinutes();
+//                     if (time == 50) {
+//                         console.log('doot doot');
+//                         const guildRole = message.guild.roles.cache.find(r => r.name === 'Doot');
+//                         message.channel.send(`${guildRole} runs beginning in 10 minutes, grab your keys and head to Limeedle 1! Don't forget to equip drop rate cards & gear if you can <:doot:825421374205722665>`);
+//                     }
+//                 }
+//             }
+//         }
+// 	},
+// };
--- events/ready.js
+++ events/ready.js
@@ -5,5 +5,45 @@ module.exports = {
 		client.user.setActivity('Idleon | ~help', { type: 'PLAYING' })
             .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
             .catch(console.error);
+
+		client.channels.fetch('823270375865188442')
+		.then(channel => {
+			setInterval(babaMessage, 60000);
+			console.log('baba timer started');
+			function babaMessage() {
+				const currentDate = new Date();
+				const time = currentDate.getMinutes();
+				if (time == 55) {
+					console.log('it\'s time');
+					channel.send(`<@&823270848252739644>`);
+					channel.send(`https://media.discordapp.net/attachments/738868424813445172/815492250209484800/spike-baba-alert3.gif`);
+				}
+				else if (time == 1) {
+					channel.bulkDelete(2, true).catch(err => {
+						console.error(err);
+					});
+				}
+			}
+		});
+
+		client.channels.fetch('823270400791150664')
+		.then(channel => {
+			setInterval(dootMessage, 60000);
+			console.log('doot timer started');
+			function dootMessage() {
+				const currentDate = new Date();
+				const dayOfWeek = currentDate.getUTCDay();
+				if (dayOfWeek == 0 || dayOfWeek == 6) {
+					const utcHour = currentDate.getUTCHours();
+					if (utcHour == 17) {
+						const time = currentDate.getMinutes();
+						if (time == 50) {
+							console.log('doot doot');
+							channel.send(`<@&825420735669469244> runs beginning in 10 minutes, grab your keys and head to Limeedle 1! Don't forget to equip drop rate cards & gear if you can <:doot:825421374205722665>`);
+						}
+					}
+				}
+			}
+		});
 	},
 };
